3194c3fc8b85def81d68420b9a54ede5a69a4f2b
mbinelli22 <mbinelli1996@gmail.com>
2019-05-04 17:09:11 -0500

    created member methods needed and a few variables

--- src/Program10/SinglePlayerPoker.java
+++ src/Program10/SinglePlayerPoker.java
@@ -2,8 +2,12 @@ package Program10;
 //Michael Binelli
 import java.util.Scanner;
 public class SinglePlayerPoker {
-	public static void main(Sting[] args) {
-		
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int []	deck = new int[36];
+		String player1;
+		int chip;
+		double chipValue = 1.00;
 	}
 	
 	public static void sortHand(int[] hand){
@@ -17,5 +21,51 @@ public class SinglePlayerPoker {
 					hand[maxLoc] = tmp;
 		}
 	}
+	
+	public static void displayHand(int[] hand) {
+		
+	}
+	
+	public static int getBet(Scanner stdIn, int bank) {
+		
+	}
+	
+	public static boolean isQuad(int[] hand) {
+		
+	}
+	
+	public static boolean isStraightFlush(int[] hand) {
+		
+	}
+	
+	public static boolean isFlush(int[] hand) {
+		
+	}
+	
+	public static boolean isStraight(int[] hand) {
+		
+	}
+	
+	public static boolean isTrip(int[] hand) {
+		
+	}
+	
+	public static boolean is2Pair(int[] hand) {
+		
+	}
+
+	public static boolean isPair(int[] hand) {
+		
+	}
+	
+	public static void report(int nHands, int nWins, int nLosses, int bank) {
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 }
